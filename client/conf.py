783b108ed47616a118cd9c6db5cc6576450fba37
BuildTools <unconfigured@null.spigotmc.org>
2021-09-27 21:41:41 +0200

    commit

--- client/conf.py
+++ client/conf.py
@@ -17,7 +17,7 @@ inventoryBoxSelectedColor = (205, 205, 205)
 textInputSelectedOutline = (184, 184, 184)
 
 APPDATA = os.getenv("APPDATA")
-GAME_DIR = str(APPDATA) + "/.rumbatale/client"
+GAME_DIR = str(APPDATA) + "/.rumbatale/"
 
-RESOURCE_DIR = GAME_DIR + "/resources"
-FONT_DIR = GAME_DIR + "/font/fonts"
+RESOURCE_DIR = GAME_DIR + "/client/resources"
+FONT_DIR = GAME_DIR + "/client/font/fonts"
