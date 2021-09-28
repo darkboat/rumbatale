c9c9499541320f11af12304ebb4f8ab0daa29f57
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 17:09:39 +0200

    hot-fix

--- push.bat
+++ push.bat
@@ -1,3 +1,14 @@
+cd client
+
+echo creating executable...
+
+pyinstaller -c -F main.spec
+cd ..
+
+echo pushing to git...
+
 git add .
 git commit -m "hot-fix"
 git push -u origin main
+
+echo finished
