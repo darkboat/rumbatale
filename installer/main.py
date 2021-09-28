78b8a31b6b71e2937d2bc4c0f03275f38dd015f5
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 15:53:39 +0200

    hot-fix

--- installer/main.py
+++ installer/main.py
@@ -17,30 +17,36 @@ def onerror(func, path, exc_info):
 appdata = os.getenv("APPDATA")
 outDir = str(appdata) + "/.rumbatale"
 
+print("deleting past install...")
+
 shutil.rmtree(outDir, ignore_errors=True)
 
 def on_rm_error(func, path, exc_info):
     os.chmod(path, stat.S_IWRITE)
     os.unlink(path)
 
-
-for i in os.listdir(outDir):
-    if i.endswith('git'):
-        tmp = os.path.join(outDir, i)
-        # We want to unhide the .git folder before unlinking it.
-        while True:
-            call(['attrib', '-H', tmp])
-            break
-        shutil.rmtree(tmp, onerror=on_rm_error)
+if os.path.exists(outDir):
+    for i in os.listdir(outDir):
+        if i.endswith('git'):
+            tmp = os.path.join(outDir, i)
+            while True:
+                call(['attrib', '-H', tmp])
+                break
+            shutil.rmtree(tmp, onerror=on_rm_error)
 
 
 while True:
-    if len(os.listdir(outDir)) == 0:
+    if (len(os.listdir(outDir)) == 0) if os.path.exists(outDir) else True:
+
         Repo.clone_from("https://github.com/darkboat/rumbatale.git", outDir)
+
+        print("cloning repository...")
         
         shutil.rmtree(outDir + "/installer") # remove unwanted folder from repository
+        
+        print("cleaning up...")
 
-        # ENDING OF INSTALLATIOn
+        # ENDING OF INSTALLATION
         break
     else:
         time.sleep(0.001)
