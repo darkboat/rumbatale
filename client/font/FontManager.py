1a175ad517e4f7b95f95aff5c6e9ee2d3bb4c27e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 15:58:40 +0200

    hot-fix

--- client/font/FontManager.py
+++ client/font/FontManager.py
@@ -3,8 +3,8 @@ import pygame
 
 class FontManager:
     def __init__(self):
-        self.hudText = pygame.font.Font(FONT_DIR + "Roboto-Black.ttf", 25)
-        self.inventoryItemText = pygame.font.Font(FONT_DIR + "Gluten-Thin.ttf", 20)
-        self.titleText = pygame.font.Font(FONT_DIR + "SigmarOne-Regular.ttf", 35)
-        self.textInput = pygame.font.Font(FONT_DIR + "Roboto-Black.ttf", 15)
-        self.damageNumbers = pygame.font.Font(FONT_DIR + "SigmarOne-Regular.ttf", 20)
+        self.hudText = pygame.font.Font("C:/Users/mikae/AppData/Roaming/.rumbatale/client/font/fonts/Roboto-Black.ttf", 25)
+        self.inventoryItemText = pygame.font.Font("C:/Users/mikae/AppData/Roaming/.rumbatale/client/font/fonts/Gluten-Thin.ttf", 20)
+        self.titleText = pygame.font.Font("C:/Users/mikae/AppData/Roaming/.rumbatale/client/font/fonts/SigmarOne-Regular.ttf", 35)
+        self.textInput = pygame.font.Font("C:/Users/mikae/AppData/Roaming/.rumbatale/client/font/fonts/Roboto-Black.ttf", 15)
+        self.damageNumbers = pygame.font.Font("C:/Users/mikae/AppData/Roaming/.rumbatale/client/font/fonts/SigmarOne-Regular.ttf", 20)
